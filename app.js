78adfb9940f1c67a68d584155bd2553f818960d2
bezawitdev <getbezawit@gmail.com>
2022-08-30 22:43:06 -0400

    database name change

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use('/books', router)
 
 
-let MONGODB_URI =   process.env.PROD_MONGODB || process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Book-DB'
+let MONGODB_URI =   process.env.PROD_MONGODB || process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/test'
 
 mongoose.connect(MONGODB_URI, {useUnifiedTopology: true, useNewUrlParser: true})
 .then(() => {
