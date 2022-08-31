709b7c5be6f8174d1f63e3f74d2ed5767280bc41
bezawitdev <getbezawit@gmail.com>
2022-08-30 23:00:25 -0400

    final commit

--- book-app/src/apiConfig.js
+++ book-app/src/apiConfig.js
@@ -1,12 +1,12 @@
 let apiUrl
 const apiUrls = {
-//  production: 'https://kavitha-mern-blog-app.herokuapp.com/api',
+production: 'https://mern-book-info-api.herokuapp.com',
   development: 'http://localhost:5000'
 }
 
 if (window.location.hostname === 'localhost') {
   apiUrl = apiUrls.development
-//   apiUrl = apiUrls.production
+//  apiUrl = apiUrls.production
 } else {
   apiUrl = apiUrls.production
 }
